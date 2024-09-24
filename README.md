2b6202738ad2f370d802fca8465b0e6ed68471a1
Will Robinson <will@oznetnerd.com>
2024-09-24 11:01:47 +1000

    Added log messages, added image tag  & updated README file

--- README.md
+++ README.md
@@ -11,6 +11,97 @@ The script only supports ECR at this stage.
 ./pull_push.sh csa_agent_openshift_standard.yaml
 ```
 
+## Example Usage
+
+```
+./pull_push.sh csa_agent_openshift_standard.yaml
+Reading YAML file: csa_agent_openshift_standard.yaml
+Decoded config value
+Extracting repo name
+Logging into Palo Alto Docker registry: australia-southeast1-docker
+Login Succeeded
+Downloading Cortex image: australia-southeast1-docker/xdr-au-1234567890123/agent-docker/cortex-agent:8.5.0.125392
+8.5.0.125392: Pulling from xdr-au-1234567890123/agent-docker/cortex-agent
+7194e565e7c0: Pull complete
+31fcb65a1fe6: Pull complete
+023217d047fc: Pull complete
+9d5071754f66: Pull complete
+ea498bc5bad8: Pull complete
+cea6af244043: Pull complete
+59a0dfe115e6: Pull complete
+b8e76181c4dc: Pull complete
+b2ff9e427df5: Pull complete
+35f91b193e5c: Pull complete
+9be7c3a9e5b4: Pull complete
+d34c9afa5873: Pull complete
+b279fe36cd49: Pull complete
+Digest: sha256:b5fd68358a2cb70ca9a9a272fda56b1f4fb87e359cde4bd787ed3dc6e20a981e
+Status: Downloaded newer image for australia-southeast1-docker/xdr-au-1234567890123/agent-docker/cortex-agent:8.5.0.125392
+australia-southeast1-docker/xdr-au-1234567890123/agent-docker/cortex-agent:8.5.0.125392
+Successfully pulled Cortex image
+Preparing to push image to AWS ECR registry
+Retrieving AWS account ID
+Retrieving AWS region
+Constructing ECR registry URL
+Constructing ECR repo name
+Checking if ECR repo already exists
+ECR repository 'cortex-agent' does not exist. Creating repository
+{
+    "repository": {
+        "repositoryArn": "arn:aws:ecr:ap-southeast-2:123456789012:repository/cortex-agent",
+        "registryId": "123456789012",
+        "repositoryName": "cortex-agent",
+        "repositoryUri": "123456789012.dkr.ecr.ap-southeast-2.amazonaws.com/cortex-agent",
+        "createdAt": "2024-09-24T10:54:02.355000+10:00",
+        "imageTagMutability": "MUTABLE",
+        "imageScanningConfiguration": {
+            "scanOnPush": false
+        },
+        "encryptionConfiguration": {
+            "encryptionType": "AES256"
+        }
+    }
+}
+Tagging image with the original tag
+Tagging image with the 'latest' tag
+Logging into ECR Registry: 123456789012.dkr.ecr.ap-southeast-2.amazonaws.com
+Login Succeeded
+Pushing image to: 123456789012.dkr.ecr.ap-southeast-2.amazonaws.com/cortex-agent:8.5.0.125392
+Using default tag: latest
+The push refers to repository [123456789012.dkr.ecr.ap-southeast-2.amazonaws.com/cortex-agent]
+b279fe36cd49: Pushed
+d34c9afa5873: Pushed
+9be7c3a9e5b4: Pushed
+35f91b193e5c: Pushed
+b2ff9e427df5: Pushed
+b8e76181c4dc: Pushed
+59a0dfe115e6: Pushed
+cea6af244043: Pushed
+ea498bc5bad8: Pushed
+9d5071754f66: Pushed
+023217d047fc: Pushed
+31fcb65a1fe6: Pushed
+7194e565e7c0: Pushed
+latest: digest: sha256:828f1af3d0dd8cf843a5027e2ba00dc046a7972b054a9b96a59c34e13c965930 size: 3023
+Image successfully pushed to ECR: 123456789012.dkr.ecr.ap-southeast-2.amazonaws.com/cortex-agent
+Pushing image to: 123456789012.dkr.ecr.ap-southeast-2.amazonaws.com/cortex-agent:latest
+The push refers to repository [123456789012.dkr.ecr.ap-southeast-2.amazonaws.com/cortex-agent]
+b279fe36cd49: Layer already exists
+d34c9afa5873: Layer already exists
+9be7c3a9e5b4: Layer already exists
+35f91b193e5c: Layer already exists
+b2ff9e427df5: Layer already exists
+b8e76181c4dc: Layer already exists
+59a0dfe115e6: Layer already exists
+cea6af244043: Layer already exists
+ea498bc5bad8: Layer already exists
+9d5071754f66: Layer already exists
+023217d047fc: Layer already exists
+31fcb65a1fe6: Layer already exists
+7194e565e7c0: Layer already exists
+latest: digest: sha256:828f1af3d0dd8cf843a5027e2ba00dc046a7972b054a9b96a59c34e13c965930 size: 3023
+Image successfully pushed to ECR: 123456789012.dkr.ecr.ap-southeast-2.amazonaws.com/cortex-agent (tags: 8.5.0.125392, latest)
+```
 ## Supporting Additional Registries
 
 Support for additional registries can be added by:
--- pull_push.sh
+++ pull_push.sh
@@ -2,7 +2,7 @@
 
 check_inputs() {
     if [[ -z "$1" ]]; then
-        echo "Error: No YAML file path provided."
+        echo "Error: No YAML file path provided"
         echo "Usage: check_inputs <path_to_yaml_file>"
         echo
         exit 1
@@ -12,75 +12,99 @@ check_inputs() {
         else
             abs_path="$1"
         fi
-        echo "Error: File '$abs_path' does not exist."
+        echo "Error: File '$abs_path' does not exist"
         echo
         exit 1
     fi
 }
 
 download_image() {
+    echo "Reading YAML file: $manifest_file"
     # Extract the dockerconfigjson value from the YAML
     docker_config_value=$(grep dockerconfigjson: "$manifest_file" | awk '{print $2}')
+
     # Decode base64
+    echo "Decoded config value"
     decoded_docker_config=$(echo "$docker_config_value" | base64 -d)
 
     # Extract repo and trim the https prefix
+    echo "Extracting repo name"
     repo=$(echo "$decoded_docker_config" | jq -r '.auths | keys[0]' | sed 's/https:\/\///')
 
     # Extract the password entry
     docker_password=$(echo "$decoded_docker_config" | jq -r '.auths[].password')
 
     # Docker login
-    echo "Docker login to $repo"
+    echo "Logging into Palo Alto Docker registry: $repo"
     echo "$docker_password" | docker login "$repo" -u "_json_key" --password-stdin
 
     # Extract the image URL from the Kubernetes manifest
     image=$(grep -A 2 "containers:" "$manifest_file" | grep "image:" | awk '{print $2}')
-    echo "Downloading image: $image"
+    echo "Downloading Cortex image: $image"
 
     # Pull image locally
     docker pull "$image"
+
+    echo "Successfully pulled Cortex image"
 }
 
 push_image_to_ecr() {
+    echo "Preparing to push image to AWS ECR registry"
+
     # Extract AWS account ID and region
+    echo "Retrieving AWS account ID"
     aws_account_id=$(aws sts get-caller-identity --query "Account" --output text)
+
     aws_region=$(aws configure get region)
+    echo "Retrieving AWS region"
 
     if [[ -z "$aws_account_id" || -z "$aws_region" ]]; then
-        echo "Error: Could not retrieve AWS account ID or region."
+        echo "Error: Could not retrieve AWS account ID and/or region"
         exit 1
     fi
 
-    # Extract the image URL from the Kubernetes manifest
-    image=$(grep -A 2 "containers:" "$manifest_file" | grep "image:" | awk '{print $2}')
-
     # Define the ECR repository URL
+    echo "Constructing ECR registry URL"
     ecr_repo_url="$aws_account_id.dkr.ecr.$aws_region.amazonaws.com"
+
+    # Extract image name and tag
+    echo "Constructing ECR repo name"
     repo_name=$(basename "$image" | cut -d':' -f1) # Get the image name without tag
+    image_tag=$(basename "$image" | cut -d':' -f2) # Extract the tag from the image
 
     # Check if the ECR repository exists
+    echo "Checking if ECR repo already exists"
     repo_exists=$(aws ecr describe-repositories --repository-names "$repo_name" --region "$aws_region" 2>/dev/null)
 
     if [[ -z "$repo_exists" ]]; then
         # Create the ECR repository if it does not exist
-        echo "ECR repository '$repo_name' does not exist. Creating repository."
+        echo "ECR repository '$repo_name' does not exist. Creating repository"
         aws ecr create-repository --repository-name "$repo_name" --region "$aws_region"
     else
-        echo "ECR repository '$repo_name' already exists."
+        echo "ECR repository '$repo_name' already exists"
     fi
 
-    # Tag the image for ECR
-    docker tag "$image" "$ecr_repo_url/$repo_name"
+    # Tag the image for ECR with the original tag
+    echo "Tagging image with the original tag"
+    docker tag "$image" "$ecr_repo_url/$repo_name:$image_tag"
+
+    # Tag the image with the 'latest' tag
+    echo "Tagging image with the 'latest' tag"
+    docker tag "$image" "$ecr_repo_url/$repo_name:latest"
 
     # Authenticate Docker to the ECR registry
+    echo "Logging into ECR Registry: $ecr_repo_url"
     aws ecr get-login-password --region "$aws_region" | docker login --username AWS --password-stdin "$ecr_repo_url"
 
-    # Push the image to ECR
-    echo "Pushing image to $ecr_repo_url/$repo_name"
-    docker push "$ecr_repo_url/$repo_name"
-    echo "Image successfully pushed to ECR: $ecr_repo_url/$repo_name"
+    # Push the image to ECR with the original tag
+    echo "Pushing image to: $ecr_repo_url/$repo_name:$image_tag"
+    docker push "$ecr_repo_url/$repo_name:$image_tag"
 
+    # Push the image to ECR with the 'latest' tag
+    echo "Pushing image to: $ecr_repo_url/$repo_name:latest"
+    docker push "$ecr_repo_url/$repo_name:latest"
+
+    echo "Image successfully pushed to ECR: $ecr_repo_url/$repo_name (tags: $image_tag, latest)"
 }
 
 manifest_file="$1"
